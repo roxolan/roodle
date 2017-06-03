b83f0cffbb8c3efa6447ae0d0ef5aef06831162f
Viktor Kotusenko <roxolanus@gmail.com>
2017-06-02 23:50:23 -0400

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    app: './src/App.jsx',
+    vendor: ['react', 'react-dom', 'react-router', 'babel-polyfill']
+  },
+  output: {
+    path: path.join(__dirname, '/dist'),
+    filename: 'app.bundle.js'
+  },
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({name: 'vendor', filename: 'vendor.bundle.js'})
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['react', 'es2015']
+          }
+        }
+      },
+      {
+        test: /\.html$/,
+        use: {
+          loader: 'file-loader'
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  stats: {
+    colors: true,
+    reasons: true,
+    chunks: false
+  },
+  devServer: {
+    port: 8000,
+    contentBase: path.join(__dirname, '/dist'),
+    historyApiFallback: true
+  },
+  devtool: 'source-map'
+}
